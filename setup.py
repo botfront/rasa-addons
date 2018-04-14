ab505b174b2c5f307aa95a2ac409fc9011579b8f
Nathan Zylbersztejn <nathan@mrbot.ai>
2018-04-13 20:13:22 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name='mrbot_rasa_addons',
     description='Rasa Add-ons',
-    version='0.1',
+    version='0.1.1',
     author='Nathan Zylbersztejn',
     install_requires=['flask', 'flask_socketio', 'flask_cors'],
     licence='Apache 2.0',
     url='http://mrbot.ai',
     author_email='human@mrbot.ai',
-    packages=['rasa_addons', 'rasa_addons.webchat']
+    packages=['rasa_addons', 'rasa_addons.webchat', 'rasa_addons.superagent']
 )
