e860d2bcc056810e5892ea24dc5481e5f5b84240
Nathan Zylbersztejn <nathan@mrbot.ai>
2018-06-05 10:37:11 -0400

    compatibility core 0.9

--- rasa_addons/superagent/__init__.py
+++ rasa_addons/superagent/__init__.py
@@ -2,7 +2,6 @@ import os
 from rasa_addons.superagent.message_processor import SuperMessageProcessor
 from rasa_core.agent import Agent
 from rasa_core.domain import TemplateDomain
-from rasa_core.featurizers import Featurizer
 from rasa_core.interpreter import NaturalLanguageInterpreter
 from rasa_core.policies.ensemble import PolicyEnsemble
 from rasa_core.processor import MessageProcessor
@@ -13,7 +12,6 @@ class SuperAgent(Agent):
             self,
             domain,  # type: Union[Text, Domain]
             policies=None,  # type: Optional[Union[PolicyEnsemble, List[Policy]]
-            featurizer=None,  # type: Optional[Featurizer]
             interpreter=None,  # type: Optional[NaturalLanguageInterpreter]
             tracker_store=None,  # type: Optional[TrackerStore]
             rules_file=None  # type: Optional[str]
@@ -23,14 +21,17 @@ class SuperAgent(Agent):
         super(SuperAgent, self).__init__(
             domain,
             policies,
-            featurizer,
             interpreter,
             tracker_store
         )
 
     @classmethod
-    def load(cls, path, interpreter=None, tracker_store=None,
-             action_factory=None, rules_file=None):
+    def load(cls,
+             path,
+             interpreter=None,
+             tracker_store=None,
+             action_factory=None,
+             rules_file=None):
         # type: (Text, Any, Optional[TrackerStore]) -> Agent
 
         if path is None:
@@ -39,11 +40,10 @@ class SuperAgent(Agent):
                                      action_factory)
         # ensures the domain hasn't changed between test and train
         domain.compare_with_specification(path)
-        featurizer = Featurizer.load(path)
-        ensemble = PolicyEnsemble.load(path, featurizer)
+        ensemble = PolicyEnsemble.load(path)
         _interpreter = NaturalLanguageInterpreter.create(interpreter)
         _tracker_store = cls.create_tracker_store(tracker_store, domain)
-        return cls(domain, ensemble, featurizer, _interpreter, _tracker_store, rules_file)
+        return cls(domain, ensemble, _interpreter, _tracker_store, rules_file)
 
     def _create_processor(self, preprocessor=None):
         # type: (Callable[[Text], Text]) -> MessageProcessor
