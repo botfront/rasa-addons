8d3970b07059e3ce4e8d16508229c1714174f12f
TheoTomalty <theo@tomalty.com>
2018-12-03 10:05:41 -0500

    version change

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with io.open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md
     long_description = f.read()
 setup(
     name='rasa_addons',
-    version='0.5.2',
+    version='0.5.5',
     author='Nathan Zylbersztejn',
     description="Rasa Addons - Productivity tools for Rasa Core",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=['jsonmerge', 'pyyaml', 'schema', 'rasa_core>=0.11.0'],
     licence='Apache 2.0',
-    url='http://mrbot.ai',
+    url='https://mrbot.ai',
     author_email='human@mrbot.ai',
     packages=['rasa_addons']
 )
