ded6e891be5e013d926875d3cfe46d1f96585bbe
Nathan Zylbersztejn <nathan@mrbot.ai>
2019-06-15 14:55:28 -0400

    refactor: fixed issues with previous merge

--- rasa_addons/core/policies/__init__.py
+++
@@ -1,4 +0,0 @@
-<<<<<<< HEAD
-from rasa_addons.core.policies.botfront_mapping_policy import BotfrontMappingPolicy
-=======
->>>>>>> parent of 40cc196... Revert "refactor: renamed/restructured components consistently with Rasa components structure"
