89619e95d616162096f68a62798d0120501bcc2e
Philippe Cote-Boucher <philippe@mrbot.ai>
2019-09-03 14:26:03 -0400

    feat(disambiguation): wip

--- rasa_addons/core/policies/disambiguation.py
+++ rasa_addons/core/policies/disambiguation.py
@@ -2,18 +2,15 @@ import json
 import logging
 import os
 from typing import Any, List, Text, Dict
-
-from rasa.core.actions.action import ACTION_LISTEN_NAME
-# from schema import Schema, And, Optional
 import rasa.utils.io
 
 from rasa.core import utils
 from rasa.core.domain import Domain
-from rasa.core.policies.policy import Policy
+from rasa.core.policies.policy import Policy, confidence_scores_for
+from rasa.core.events import SlotSet
 from rasa.core.trackers import DialogueStateTracker
 
 logger = logging.getLogger(__name__)
-
 
 class DisambiguationPolicy(Policy):
     """Policy which predicts fallback actions.
@@ -30,31 +27,28 @@ class DisambiguationPolicy(Policy):
         self,
         priority: int = 4,
         disambiguation_trigger: str = "$0 < 2 * $1",
-        fallback_trigger: str = "$0 <= 0.30",
-        disambiguation_action_name: Text = "action_default_disambiguation",
-        fallback_action_name: Text = "action_default_fallback",
-        exclude_intents: List = [], # list of regex e.g ["chitchat.*", "basics.*",...]
-
+        fallback_trigger: float = 0.30,
+        excluded_intents: List = [], # list of regex e.g ["chitchat.*", "basics.*",...]
+        intent_mappings: Dict[Text, Dict[Text, Text]] = {}, # {"basics.yes": {"en": "Yes", "fr": "Oui"}}
+        n_suggestions: int = 3,
+        disambiguation_title: Dict[Text, Text] = {
+            "en": "Sorry, I'm not sure I understood. Did you mean..."
+        }
     ) -> None:
-        """Create a new Fallback policy.
-
-        Args:
-            core_threshold: if NLU confidence threshold is met,
-                predict fallback action with confidence `core_threshold`.
-                If this is the highest confidence in the ensemble,
-                the fallback action will be executed.
-            nlu_threshold: minimum threshold for NLU confidence.
-                If intent prediction confidence is lower than this,
-                predict fallback action with confidence 1.0.
-            fallback_action_name: name of the action to execute as a fallback
-        """
         super(DisambiguationPolicy, self).__init__(priority=priority)
 
-
-        self.disambiguation_action_name = disambiguation_action_name
-        self.fallback_action_name = fallback_action_name
         self.disambiguation_trigger = disambiguation_trigger
         self.fallback_trigger = fallback_trigger
+        self.fallback_default_confidence = 0.30
+        self.disambiguation_action = "action_botfront_disambiguation"
+        self.disambiguation_followup_action = "action_botfront_disambiguation_followup"
+        self.disambiguation_action_denial = "action_botfront_disambiguation_denial" # utter_ask_rephrase
+        self.fallback_action = "action_botfront_fallback" # utter_fallback
+        self.deny_suggestion_intent_name = "deny_suggestions"
+        self.excluded_intents = excluded_intents + [self.deny_suggestion_intent_name]
+        self.n_suggestions = n_suggestions
+        self.intent_mappings = intent_mappings
+        self.disambiguation_title = disambiguation_title
 
     def train(
         self,
@@ -62,22 +56,68 @@ class DisambiguationPolicy(Policy):
         domain: Domain,
         **kwargs: Any
     ) -> None:
-        """Does nothing. This policy is deterministic."""
+        pass
 
-        print("train")
+    def generate_disambiguation_message(self, tracker):
+        language = tracker.latest_message.parse_data.get('language', 'en')
+        intent_ranking = tracker.latest_message.parse_data.get('intent_ranking', [])
+        intent_ranking = [intent for intent in intent_ranking
+                          if intent.get('name', '') not in self.excluded_intents][:self.n_suggestions]
+                              
+        first_intent_names = [intent.get('name', '')
+                              for intent in intent_ranking]
 
-    def should_disambiguate(self, parse_data, last_action_name):
-        if not self.disambiguation_trigger or "intent_ranking" not in parse_data:
-            return False
+        mapped_intents = [(name, self.intent_mappings.get(name, {language: name})[language])
+                          for name in first_intent_names]
 
-        return (
-            last_action_name == ACTION_LISTEN_NAME
-            and self.is_triggered(parse_data, self.disambiguation_trigger)
-            and not self.is_triggered(parse_data, self.fallback_trigger)
-            )
+        entities = tracker.latest_message.parse_data.get("entities", [])
+        entities_json, entities_text = self.get_formatted_entities(entities)
+
+        message_title = self.disambiguation_title["en"]
+        deny_text = self.intent_mappings.get(self.deny_suggestion_intent_name, {"en": "Something else"})[language]
+
+        buttons = []
+        for intent in mapped_intents:
+            buttons.append({'title': intent[1] + entities_text,
+                            'payload': '/{}{}'.format(intent[0],
+                                                      entities_json)})
+
+        buttons.append({'title': deny_text,
+                        'payload': '/{}'.format(self.deny_suggestion_intent_name)})
+        return {
+            "title": message_title,
+            "buttons": buttons
+        }
 
     @staticmethod
-    def is_triggered(parse_data, trigger):
+    def set_slot(tracker, message):
+        try:
+            tracker.update(SlotSet("disambiguation_message", value=message))
+            result = message
+        except Exception as e:
+            logger.error("Could not set message slot: {}".format(e))
+            result = None
+        return result
+
+    @staticmethod
+    def get_formatted_entities(entities: List[Dict[str, Any]]) -> (Text, Text):
+        key_value_entities = {}
+        for e in entities:
+            key_value_entities[e.get("entity")] = e.get("value")
+        entities_json = ""
+        entities_text = ""
+        if len(entities) > 0:
+            entities_json = json.dumps(key_value_entities)
+            entities_text = ["'{}': '{}'".format(k, key_value_entities[k])
+                            for k in key_value_entities]
+            entities_text = ", ".join(entities_text)
+            entities_text = " ({})".format(entities_text)
+
+        return entities_json, entities_text
+
+    @staticmethod
+    def _should_disambiguate(parse_data, trigger):
+        #if not len(parse_data["intent_ranking"]): return False
         import re
         # pattern to match $0, $1, $2, ... and returning 0, 1, 2,... in match groups
         pattern = re.compile(r"\$(\d)")
@@ -86,68 +126,68 @@ class DisambiguationPolicy(Policy):
         matches = re.findall(pattern, trigger)
         for i in matches:
             # if not enough intents in ranking to apply the rule, policy rule can't be triggered
-            if int(i) >= len(parse_data["intent_ranking"]):
-                return False
+            if int(i) >= len(parse_data["intent_ranking"]): return False
             eval_string = re.sub(r'\$' + i, str(parse_data["intent_ranking"][int(i)]["confidence"]), eval_string)
 
-        return len(parse_data["intent_ranking"]) and eval(eval_string, {'__builtins__': {}})
+        return eval(eval_string, {'__builtins__': {}})
 
-    def should_fallback(
-        self, parse_data, last_action_name
-    ) -> bool:
-        """Checks if fallback action should be predicted.
+    def _is_user_input_expected(self, tracker: DialogueStateTracker) -> bool:
+        return tracker.latest_action_name in [
+            self.fallback_action,
+            self.disambiguation_action,
+            self.disambiguation_action_denial
+        ]
+    
+    def _have_options_been_suggested(self, tracker: DialogueStateTracker) -> bool:
+        return tracker.last_executed_action_has(self.disambiguation_action)
 
-        Checks for:
-        - predicted NLU confidence is lower than ``nlu_threshold``
-        - last action is action listen
-        """
-
-        return (
-                last_action_name == ACTION_LISTEN_NAME
-                and self.is_triggered(parse_data, self.fallback_trigger)
-        )
-
-    def disambiguation_scores(self, domain, disambiguation_score=1.0):
-        """Prediction scores used if a fallback is necessary."""
-
-        result = [0.0] * domain.num_actions
-        idx = domain.index_for_action(self.disambiguation_action_name)
-        result[idx] = disambiguation_score
-        return result
-
-    def fallback_scores(self, domain, fallback_score=1.0):
-        """Prediction scores used if a fallback is necessary."""
-
-        result = [0.0] * domain.num_actions
-        idx = domain.index_for_action(self.fallback_action_name)
-        result[idx] = fallback_score
-        return result
+    def _has_user_denied(self, parse_data) -> bool:
+        last_intent = parse_data["intent"].get("name", None)
+        return last_intent == self.deny_suggestion_intent_name
 
     def predict_action_probabilities(
         self, tracker: DialogueStateTracker, domain: Domain
     ) -> List[float]:
-        """Predicts a fallback action.
 
-        The fallback action is predicted if the NLU confidence is low
-        or no other policy has a high-confidence prediction.
-        """
+        parse_data = tracker.latest_message.parse_data
+        can_apply = tracker.latest_action_name == "action_listen"
+        should_fallback = can_apply and parse_data["intent"]["confidence"] < self.fallback_trigger
+        should_disambiguate = can_apply and self._should_disambiguate(
+            parse_data, self.disambiguation_trigger
+        )
 
-        nlu_data = tracker.latest_message.parse_data
+        if self._is_user_input_expected(tracker):
+            # Shut up and listen
+            result = confidence_scores_for("action_listen", 1.0, domain)
 
-        if tracker.latest_action_name == self.disambiguation_action_name:
-            result = [0.0] * domain.num_actions
-            idx = domain.index_for_action(ACTION_LISTEN_NAME)
-            result[idx] = 1.0
+        elif self._has_user_denied(parse_data):
+            logger.debug("User '{}' denied suggested intents.".format(tracker.sender_id))
+            result = confidence_scores_for(self.disambiguation_action_denial, 1.0, domain)
 
-        elif self.should_disambiguate(nlu_data, tracker.latest_action_name):
-            logger.debug("Disambiguation triggered")
+        elif should_fallback:
+            logger.debug("Triggering fallback")
+            result = confidence_scores_for(self.fallback_action, 1.0, domain)
+        
+        elif self._have_options_been_suggested(tracker):
+            if not should_disambiguate:
+                logger.debug("Successfully disambiguated")
+                result = confidence_scores_for(self.disambiguation_followup_action, 1.0, domain)
+            else:
+                logger.debug("Will not disambiguate a second time so fast -- triggering fallback")
+                result = confidence_scores_for(self.fallback_action, 1.0, domain)
 
-            result = self.disambiguation_scores(domain)
+        elif should_disambiguate:
+            logger.debug("Triggering disambiguation")
+            disambiguation_message = self.generate_disambiguation_message(tracker)
+            slot_set = self.set_slot(tracker, disambiguation_message)
+            if slot_set:
+                result = confidence_scores_for(self.disambiguation_action, 1.0, domain)
+            else:
+                result = confidence_scores_for(self.fallback_action, 1.0, domain)
 
-        elif self.should_fallback(nlu_data, tracker.latest_action_name):
-            logger.debug("Fallback triggered")
-
-            result = self.fallback_scores(domain)
+        else:
+            # Nothing to see here; setting fallback to default confidence
+            result = confidence_scores_for(self.fallback_action, self.fallback_default_confidence, domain)
 
         return result
 
@@ -157,10 +197,12 @@ class DisambiguationPolicy(Policy):
         config_file = os.path.join(path, "disambiguation_policy.json")
         meta = {
             "priority": self.priority,
-            "fallback_trigger": self.disambiguation_trigger,
             "disambiguation_trigger": self.disambiguation_trigger,
-            "disambiguation_action_name": self.disambiguation_action_name,
-            "fallback_action_name": self.fallback_action_name,
+            "fallback_trigger": self.fallback_trigger,
+            "excluded_intents": self.excluded_intents,
+            "n_suggestions": self.n_suggestions,
+            "intent_mappings": self.intent_mappings,
+            "disambiguation_title": self.disambiguation_title
         }
         rasa.utils.io.create_directory_for_file(config_file)
         utils.dump_obj_as_json_to_file(config_file, meta)
---
+++ rasa_addons/custom_actions/action_botfront_disambiguation.py
@@ -0,0 +1,88 @@
+from rasa_sdk import Action
+from rasa_sdk.events import SlotSet, ReminderScheduled, UserUtteranceReverted, ActionExecuted, UserUttered
+
+from typing import Any, List, Text, Dict, Optional
+import logging
+import json
+import copy
+import re
+
+from requests.auth import HTTPBasicAuth
+
+logging.basicConfig(level="WARN")
+logger = logging.getLogger()
+
+class ActionBotfrontDisambiguation(Action):
+    def name(self):
+        return "action_botfront_disambiguation"
+
+    def run(self,
+            dispatcher: "CollectingDispatcher",
+            tracker: "Tracker",
+            domain: Dict[Text, Any]
+            ) -> List["Event"]:
+        message = None
+        for event in tracker.events[::-1]:
+            if event.get("name", None) == "disambiguation_message":
+                message = event["value"]
+                break
+        if message:
+            dispatcher.utter_button_message(message["title"], buttons=message["buttons"])
+
+        return []
+
+class ActionBotfrontDisambiguationFollowup(Action):
+    def name(self) -> Text:
+        return "action_botfront_disambiguation_followup"
+
+    def run(self,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: Dict[Text, Any]
+    ) -> List["Event"]:
+
+        revert_events = [
+            UserUtteranceReverted(),
+            UserUtteranceReverted(),
+            ActionExecuted(action_name="action_listen"),
+        ]
+
+        last_user_event = None
+        for event in tracker.events[::-1]:
+            if event["event"] == "user":
+                last_user_event = copy.deepcopy(event)
+                last_user_event["parse_data"]["intent"]["confidence"] = 1.0
+                break
+        if last_user_event:
+            revert_events += [last_user_event]
+
+        return revert_events
+
+
+class ActionBotfrontDisambiguationDenial(Action):
+    def name(self) -> Text:
+        return "action_botfront_disambiguation_denial"
+
+    def run(self,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: Dict[Text, Any]
+    ) -> List["Event"]:
+        dispatcher.utter_template("utter_ask_rephrase", tracker)
+
+class ActionBotfrontFallback(Action):
+    def name(self) -> Text:
+        return "action_botfront_fallback"
+
+    def run(self,
+        dispatcher: "CollectingDispatcher",
+        tracker: "Tracker",
+        domain: Dict[Text, Any]
+    ) -> List["Event"]:
+        dispatcher.utter_template("utter_fallback", tracker)
+        if (len(tracker.events) >= 4 and
+        tracker.events[-4].get("name") ==
+        "action_botfront_disambiguation"):
+            return [UserUtteranceReverted(), UserUtteranceReverted()]
+        else:
+            return [UserUtteranceReverted()]
