4f4ef0ddf30c32db09efecf6576c37c14be8573b
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-03-27 12:24:17 -0400

    chore: release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with io.open(
     long_description = f.read()
 setup(
     name="rasa_addons",
-    version="0.9.12",
+    version="0.9.13",
     author="Botfront",
     description="Rasa Addons - Components for Rasa and Botfront",
     long_description=long_description,
