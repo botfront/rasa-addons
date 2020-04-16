035ef8581144b1b8eb508841d1490a1c92383eff
Philippe Cote-Boucher <philippe@mrbot.ai>
2020-04-16 15:08:47 -0400

    chore: version bump

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with io.open(
     long_description = f.read()
 setup(
     name="rasa_addons",
-    version="0.9.16",
+    version="0.9.17",
     author="Botfront",
     description="Rasa Addons - Components for Rasa and Botfront",
     long_description=long_description,
